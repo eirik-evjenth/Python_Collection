d76452581c997a56a0f89334ec4bbae6e53347c1
Krypton70 <epilog@gmail.com>
2025-02-17 17:01:49 +0100

    Prosjekt analyse

--- Visual Studio IT 2/Kapittel 3/Kapittel 3B/Prosjekt/get_stocks.py
+++ Visual Studio IT 2/Kapittel 3/Kapittel 3B/Prosjekt/get_stocks.py
@@ -1,7 +1,8 @@
 import yfinance as yf
 
 
-data = yf.download("AAPL", start="1980-12-12", end="2024-02-13")
+data = yf.download("AAPL", start="1980-12-12", end="2025-02-13")
+spy_data = yf.download('SPY', start='1980-12-12', end='2025-02-13')
 
-
+spy_data.to_csv("S&P 500.csv")
 data.to_csv("AAPL_max.csv")
